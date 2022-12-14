1dc209a6b5bf8acc02a72062eb6450a049755f29
Siarhei Kupryk <siarhei.kupryk@itdep.net>
2022-12-14 18:38:44 +0300

    Version 01

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-# Put your code here
+#checking if the argument has been provided:
+[ $# -ne 1 ] && echo "Provide one string as an argumnet. You've provided $# argument(s)!" && exit 1
+
+inputStr=$1
+inputStrLen=${#inputStr}
+
+while (( $inputStrLen > 0 )); do
+    let "inputStrLen -= 1"
+    resultStr="$resultStr${inputStr:$inputStrLen:1}"
+done
+
+echo $resultStr | tr 'a-zA-Z' 'A-Za-z'
